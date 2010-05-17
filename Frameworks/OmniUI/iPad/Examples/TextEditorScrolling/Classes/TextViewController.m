2d5e351befceaa754d6534b1e32aea37105bd0ea
Jeffrey Sambells <jeff@sambells.info>
2010-05-17 14:08:43 -0400

    Created a Scrolling Example that isn't working yet.

---
+++ Frameworks/OmniUI/iPad/Examples/TextEditorScrolling/Classes/TextViewController.m
@@ -0,0 +1,71 @@
+// Copyright 2010 The Omni Group.  All rights reserved.
+//
+// This software may only be used and reproduced according to the
+// terms in the file OmniSourceLicense.html, which should be
+// distributed with this project and can also be found at
+// <http://www.omnigroup.com/developer/sourcecode/sourcelicense/>.
+
+#import "TextViewController.h"
+
+#import <OmniUI/OUIEditableFrame.h>
+
+#import "RTFDocument.h"
+
+RCS_ID("$Id$");
+
+@implementation TextViewController
+
+- initWithDocument:(RTFDocument *)document;
+{
+    if (!(self = [super initWithNibName:@"TextViewController" bundle:nil]))
+        return nil;
+    
+    _nonretained_document = document;
+    
+    return self;
+}
+
+- (void)dealloc;
+{
+    [_editor release];
+    [super dealloc];
+}
+
+@synthesize editor = _editor;
+
+#pragma mark -
+#pragma mark UIViewController subclass
+
+- (void)viewDidLoad;
+{
+    [super viewDidLoad];
+	[(UIScrollView *)[self view] setContentSize:_editor.frame.size];
+    _editor.delegate = self;
+    _editor.attributedText = _nonretained_document.text;
+}
+
+- (void)viewDidUnload;
+{
+    self.editor = nil;
+    [super viewDidUnload];
+}
+
+#pragma mark OUIEditableFrameDelegate
+
+- (void)textViewContentsChanged:(OUIEditableFrame *)textView;
+{
+	//[(UIScrollView *)[self view] setContentSize:textView.frame.size];
+}
+
+- (void)textViewLayoutChanged:(OUIEditableFrame *)textView;
+{
+	//[(UIScrollView *)[self view] setContentSize:textView.frame.size];
+}
+
+- (void)textViewDidEndEditing:(OUIEditableFrame *)textView;
+{
+    // We need more of a text storage model so that selection changes can participate in undo.
+    _nonretained_document.text = textView.attributedText;
+}
+
+@end
